2aa3fe72581cdc19bf89fc6267a7120e2082df42
Ahsan <ahsanuddin160@gmail.com>
2023-10-13 07:20:33 -0700

    commit

--- tic-tac-teo/src/Todo.js
+++ tic-tac-teo/src/Todo.js
@@ -5,6 +5,11 @@ function Todo() {
   const [todos, setTodos] = useState([]);
   const [selectedUser, setSelectedUser] = useState(null);
   const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false); 
+
+  // let vs const
+  // hoisting in javascript
+  // event loop
 
   const fetchUserData = () => {
     fetch("https://jsonplaceholder.typicode.com/users/")
@@ -14,10 +19,12 @@ function Todo() {
       })
       .catch((error) => {
         setError("An error occurred while fetching user data.");
-      });
+      })
+      
   };
 
   const fetchTodosData = (userId) => {
+    setLoading(true); 
     fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
       .then((response) => response.json())
       .then((data) => {
@@ -25,12 +32,19 @@ function Todo() {
       })
       .catch((error) => {
         setError("An error occurred while fetching comments.");
+      })
+      .finally(() => {
+        console.log(1)
+        setTimeout(function () {
+          console.log(2)
+          setLoading(false); 
+        }, 3000)
+        console.log(3)
       });
   };
 
   useEffect(() => {
     fetchUserData();
-    fetchTodosData();
   }, []);
 
   const handleClick = (userId) => {
@@ -45,21 +59,19 @@ function Todo() {
 
   return (
     <div className="bg-gray-200 min-h-screen flex justify-center">
- 
-      
-
-   
-    <div class="flex flex-col justify-between p-9 leading-normal">
-    <div className="bg-white rounded-lg flex items-center justify-center">
-  <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
-    About User
-  </h5>
-</div>
-        <p class="mb-3 font-normal text-black dark:text-black"> <ul className="mt-4">
-        {userData.map((user) => (
-          <li key={user.id} className="mb-6 p-14 rounded-lg bg-white shadow">
-            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">User Informotion</h5>
-            <p>
+      <div class="flex flex-col justify-between p-9 leading-normal">
+        <div className="bg-white rounded-lg flex items-center justify-center">
+          <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">
+            About User
+          </h5>
+        </div>
+        <p class="mb-3 font-normal text-black dark:text-black">
+          <ul className="mt-4">
+            {userData.map((user) => (
+              <li key={user.id} className="mb-6 p-14 rounded-lg bg-white shadow">
+                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">User Information</h5>
+                
+                <p>
               <strong>Name:</strong> {user.name}
             </p>
             <p>
@@ -85,44 +97,39 @@ function Todo() {
             <p>
               {user.company.catchPhrase} {user.company.bs}
             </p>
-            <button
-              className={`p-1 m-1 rounded cursor-pointer ${
-                selectedUser === user.id
-                  ? "bg-red-500 text-black  hover:bg-blue-600 hover:text-white"
-                  : "bg-blue-500 text-white hover:bg-red-600 hover:text-black"
-              }`}
-              onClick={() => handleClick(user.id)}
-            >
-              {selectedUser === user.id ? "Hide Todo" : "Show Todo"}
-            </button>
-            {selectedUser === user.id && (
-              <ul>
-                {todos.map((todo) => (
-                  <li
-                    key={todo.id}
-                    className="mb-2 p-2 rounded-lg bg-white shadow"
-                  >
-                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Todo Informotion</h5>
-                    <div>User ID: {todo.userId}</div>
-                    <div>ID: {todo.id}</div>
-                    <div>Title: {todo.title}</div>
-                    <div>Completed: {todo.completed.toString()}</div>
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ul>.</p>
+                <button
+                  className={`p-1 m-1 rounded cursor-pointer ${
+                    selectedUser === user.id
+                      ? "bg-red-500 text-black  hover-bg-blue-600 hover-text-white"
+                      : "bg-blue-500 text-white hover-bg-red-600 hover-text-black"
+                  }`}
+                  onClick={() => handleClick(user.id)}
+                  disabled={loading} 
+                >
+                  {selectedUser === user.id ? "Hide Todo" : "Show Todo"}
+                </button>
+                {selectedUser === user.id && (
+                  <ul>
+                    {todos.map((todo) => (
+                      <li
+                        key={todo.id}
+                        className="mb-2 p-2 rounded-lg bg-white shadow"
+                      >
+                        <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Todo Information</h5>
+                        <div>User ID: {todo.userId}</div>
+                        <div>ID: {todo.id}</div>
+                        <div>Title: {todo.title}</div>
+                        <div>Completed: {todo.completed.toString()}</div>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            ))}
+          </ul>
+        </p>
+      </div>
     </div>
-
-
-      
-
-
-   
-    </div>
-    
   );
 }
 
