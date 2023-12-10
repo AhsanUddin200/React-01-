83481388ab4947dd50dc876db2cd13ed92ba7434
Ahsan <ahsanuddin160@gmail.com>
2023-12-10 11:39:53 -0800

    Modification

--- tic-tac-teo/src/Stopwatch.js
+++ tic-tac-teo/src/Stopwatch.js
@@ -1,32 +1,35 @@
 import React, { useState, useEffect } from 'react';
 
-
 const Stopwatch = () => {
     const [isRunning, setIsRunning] = useState(false);
     const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
 
     useEffect(() => {
-        let Timeinterval;
+        let timeInterval;
 
         if (isRunning) {
-            Timeinterval = setInterval(() => {
+            timeInterval = setInterval(() => {
                 setCurrentTime(new Date().toLocaleTimeString());
             }, 1000);
         } else {
-            clearInterval(Timeinterval);
+            clearInterval(timeInterval);
         }
 
-        return () => clearInterval(Timeinterval);
+        return () => clearInterval(timeInterval);
     }, [isRunning]);
 
-    const ChangeTime = () => {
+    const changeTime = () => {
         setIsRunning(!isRunning);
     }
 
     return (
-        <div className="text-center mt-4"> 
+        
+        <div className="text-center mt-4 bg-gradient-to-b from-cyan-200 to-gray-800 bg-gray-400 p-4 rounded-xl max-w-md mx-auto">
             <h1 className="text-3xl font-bold mb-2">{currentTime}</h1>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={ChangeTime}>
+            <button
+                className={`bg-${isRunning ? 'red' : 'blue'}-500 hover:bg-${isRunning ? 'red' : 'blue'}-700 text-white font-bold py-2 px-4 rounded`}
+                onClick={changeTime}
+            >
                 {isRunning ? "Stop" : "Start"}
             </button>
         </div>
@@ -34,57 +37,3 @@ const Stopwatch = () => {
 }
 
 export default Stopwatch;
-
-// import React, { useState, useEffect } from 'react';
-
-// function App() {
-//   const [products, setProducts] = useState([]);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     fetchData();
-//   }, []);
-
-//   const fetchData = () => {
-//     fetch('http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline')
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error('Network response was not ok');
-//         }
-//         return response.json();
-//       })
-//       .then((productData) => {
-//         setProducts(productData);
-//       })
-//       .catch((err) => {
-//         setError(err);
-//       });
-//   };
-
-//   return (
-//     <div>
-//       <h1>Maybelline Products</h1>
-//       {error ? (
-//         <p>Error: {error.message}</p>
-//       ) : products.length > 0 ? (
-//         <ul>
-//           {products.map((product) => (
-//             <li key={product.id}>
-//                 <h2><strong>Product brand:</strong>  {product.brand}</h2>
-//                 <h2><strong>Product ID:</strong>  {product.id}</h2>
-//               <h2><strong>Product Name:</strong>  {product.name}</h2>
-//               <p><strong>Price of product: </strong>{product.price}</p>
-        
-
-//               <img src={product.image_link} alt={product.name} />
-//             </li>
-//           ))}
-//         </ul>
-//       ) : (
-//         <p>Loading products...</p>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default App;
